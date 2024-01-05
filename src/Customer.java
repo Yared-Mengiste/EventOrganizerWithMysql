b278c5cd0b6e6735fc7826f6159e65988f3ea39b
Yared Mengiste <yaredmengiste01@gmail.com>
2024-01-05 16:29:40 +0300

    DataBase_1st

---
+++ src/Customer.java
@@ -0,0 +1,14 @@
+public class Customer extends Person{
+
+    /**
+     * Guest is used to initialize
+     *
+     * @param firstName is used to initialize firstName
+     * @param lastName  is used to initialize lastName
+     * @param phoneNo1  is used to initialize phoneNo1
+     * @param phoneNo2  is used to initialize phoneNo2
+     */
+    public Customer(String firstName, String lastName, String phoneNo1, String phoneNo2) {
+        super(firstName, lastName, phoneNo1, phoneNo2);
+    }
+}
